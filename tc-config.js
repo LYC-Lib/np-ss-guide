2309742a376cc6c37ca4ddd48a9b882b217fd5aa
LYC-Lib <lib_ref@np.edu.sg>
2025-08-28 16:21:43 +0800

    Interactions
    Edu Guide for non-NP users

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://bMc7uNfUOHqTpMg2SbfkIvjKDJDQ_Y8Y_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Social Sustainability Educators&#39; Guide"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Welcome to the Social Sustainability Educators&#39; Guide!&amp;nbsp;&lt;/p&gt;&lt;p&gt; &lt;/p&gt;&lt;p&gt;This guide is designed to support the integration of social sustainability into our lessons. As the topic of social sustainability (SS) is still evolving, the concepts, ideas, templates presented will be updated from time to time to keep pace with the emerging developments in this field. &amp;nbsp;The materials are meant to be a timely resource to be adapted into our teaching and learning practices.&lt;/p&gt;&lt;p&gt; &lt;/p&gt;&lt;p&gt;This guide is divided into three segments:&lt;/p&gt;&lt;ol&gt;&lt;li&gt;&lt;p&gt;&lt;strong&gt;Why&lt;em&gt;&amp;nbsp;&lt;/em&gt;&lt;/strong&gt;- the importance of learning about social sustainability and its connection to NP&#39;s strategic commitment and graduate outcomes.&lt;/p&gt;&lt;/li&gt;&lt;li&gt;&lt;p&gt;&lt;strong&gt;What&amp;nbsp;&lt;/strong&gt;- some basic concepts and NP&#39;s approach to social sustainability.&lt;/p&gt;&lt;/li&gt;&lt;li&gt;&lt;p&gt;&lt;strong&gt;How&amp;nbsp;&lt;/strong&gt;- the SS learning framework and guiding parameters to scoping learning activities.&lt;/p&gt;&lt;/li&gt;&lt;/ol&gt;&lt;p&gt;Each lesson aims to provide you with the essential content; however, additional resources are provided at the&lt;strong&gt;&amp;nbsp;&#34;LEARN MORE&#34;&lt;/strong&gt; sections if you wish to dive deeper into specific topics. Do check out the &lt;strong&gt;&#34;LESSON IDEAS&#34;&lt;/strong&gt; sections for suggestions and examples that you could include in your lessons.&lt;/p&gt;"
+};
